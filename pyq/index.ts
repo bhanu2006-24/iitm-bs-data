2f9029ad5374ccfeddb67faee61824c33c2d79f5
bhanu2006-24 <bhanupsaini2024@gmail.com>
2025-12-23 14:19:01 +0530

    added more data

--- pyq/index.ts
+++
@@ -1,5 +0,0 @@
-
-import { ExamPaper } from '../../types';
-import examsData from './exams.json';
-
-export const EXAMS: ExamPaper[] = examsData as any[]; 
--- pyq/subjects.ts
+++
@@ -1,3 +0,0 @@
-
-import subjectData from './subjects.json';
-export const SUBJECT_METADATA = subjectData; 
